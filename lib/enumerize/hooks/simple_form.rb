b7c719c2c64e14c002435993a5cde068615facba
Vasiliy Ermolovich <younash@gmail.com>
2012-03-02 10:14:26 +0300

    no need for patching SimpleForm::Input::Base

--- lib/enumerize/hooks/simple_form.rb
+++ lib/enumerize/hooks/simple_form.rb
@@ -2,17 +2,6 @@ require 'active_support/concern'
 
 module Enumerize
   module Hooks
-    module SimpleFormInputBaseExtension
-      def initialize(builder, attribute_name, column, input_type, options = {})
-        @builder = builder
-        if object.class.send(attribute_name).instance_of?(Enumerize::Attribute)
-          options.merge!(:collection => object.class.send(attribute_name).options)
-        end
-
-        super
-      end
-    end
-
     module SimpleFormBuilderExtension
       extend ActiveSupport::Concern
 
@@ -22,7 +11,8 @@ module Enumerize
 
       def input_with_enumerize(attribute_name, options={}, &block)
         if object.class.send(attribute_name).instance_of?(Enumerize::Attribute)
-          options[:as] ||= :select
+          options[:collection] ||= object.class.send(attribute_name).options
+          options[:as]         ||= :select
         end
 
         input_without_enumerize(attribute_name, options, &block)
@@ -31,5 +21,4 @@ module Enumerize
   end
 end
 
-::SimpleForm::Inputs::Base.send :include, Enumerize::Hooks::SimpleFormInputBaseExtension
 ::SimpleForm::FormBuilder.send :include, Enumerize::Hooks::SimpleFormBuilderExtension
