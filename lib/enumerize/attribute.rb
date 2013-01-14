f022b02a53fcbaa9226c0740b761bd78035a27d9
Andrew Hull <aghull@gmail.com>
2013-01-14 13:25:01 -0500

    correct AR validation using hashkeys

--- lib/enumerize/attribute.rb
+++ lib/enumerize/attribute.rb
@@ -46,12 +46,10 @@ module Enumerize
         end
 
         def #{name}=(new_value)
-          _enumerized_values_for_validation[:#{name}] = new_value.nil? ? nil : new_value.to_s
 
           allowed_value_or_nil = self.class.enumerized_attributes[:#{name}].find_value(new_value)
-          _enumerized_values_for_validation[:#{name}] = allowed_value_or_nil.to_s unless allowed_value_or_nil.nil?
+          _enumerized_values_for_validation[:#{name}] = allowed_value_or_nil.nil? ? ( new_value.nil? nil : new_value.to_s ) : allowed_value_or_nil.to_s
           allowed_value_or_nil = allowed_value_or_nil.value unless allowed_value_or_nil.nil?
-          #_enumerized_values_for_validation[:#{name}] = allowed_value_or_nil.nil? ? nil : (allowed_value_or_nil.nil? ? new_value.to_s : allowed_value_or_nil)
 
           if defined?(super)
             super allowed_value_or_nil
