ea5bc085355832b74594c6e3d8d5fdcc808f2c73
Vasiliy Ermolovich <younash@gmail.com>
2013-07-28 18:51:04 +0300

    move default value finder to the method

--- lib/enumerize/attribute.rb
+++ lib/enumerize/attribute.rb
@@ -13,13 +13,17 @@ module Enumerize
       @value_hash = Hash[@values.map { |v| [v.value.to_s, v] }]
       @value_hash.merge! Hash[@values.map { |v| [v.to_s, v] }]
 
-      if default = options[:default]
-        if default.respond_to?(:call)
-          @default_value = default
-        else
-          @default_value = find_value(default)
-          raise ArgumentError, 'invalid default value' unless @default_value
-        end
+      if options[:default]
+        @default_value = find_default_value(options[:default])
+        raise ArgumentError, 'invalid default value' unless @default_value
+      end
+    end
+
+    def find_default_value(value)
+      if value.respond_to?(:call)
+        value
+      else
+        find_value(value)
       end
     end
 
