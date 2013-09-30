52c7517e43aa6ad6ab2b4f8b6a5f18428b6059bf
Sergey Nartimov <just.lest@gmail.com>
2013-09-30 22:55:37 +0300

    use custom column coder
    Fix uniqueness validation with Active Record

--- lib/enumerize/activerecord.rb
+++ lib/enumerize/activerecord.rb
@@ -3,23 +3,30 @@ module Enumerize
     def enumerize(name, options={})
       super
 
-      if options[:scope]
-        _enumerize_module.dependent_eval do
-          if defined?(::ActiveRecord::Base) && self < ::ActiveRecord::Base
+      _enumerize_module.dependent_eval do
+        if defined?(::ActiveRecord::Base) && self < ::ActiveRecord::Base
+          if options[:scope]
             _define_scope_methods!(name, options)
+          end
 
+          # https://github.com/brainspec/enumerize/issues/74
+          unless method_defined?(:write_attribute)
             class_eval do
-              # https://github.com/brainspec/enumerize/issues/74
               def write_attribute(attr_name, value)
                 _enumerized_values_for_validation[attr_name] = value
 
                 super
               end
-
-              # Since Rails use `allocate` method on models and initializes them with `init_with` method.
-              # This way `initialize` method is not being called, but `after_initialize` callback always gets triggered.
-              after_initialize :_set_default_value_for_enumerized_attributes
             end
+          end
+
+          # Since Rails use `allocate` method on models and initializes them with `init_with` method.
+          # This way `initialize` method is not being called, but `after_initialize` callback always gets triggered.
+          after_initialize :_set_default_value_for_enumerized_attributes
+
+          # https://github.com/brainspec/enumerize/issues/111
+          unless options[:multiple]
+            serialize name, Column.new(self, name)
           end
         end
       end
@@ -42,6 +49,29 @@ module Enumerize
           values = values.map { |value| enumerized_attributes[name].find_value(value).value }
           where(arel_table[name].not_in(values))
         end
+      end
+    end
+
+    class Column
+      def initialize(klass, name)
+        @klass = klass
+        @name  = name
+      end
+
+      def dump(value)
+        if v = attr.find_value(value)
+          v.value
+        end
+      end
+
+      def load(value)
+        value
+      end
+
+      private
+
+      def attr
+        @klass.enumerized_attributes[@name]
       end
     end
   end
