a47adacef1c5f557f2debd943342ed90237c2a88
Takumi IINO <trot.thunder@gmail.com>
2017-11-30 14:51:07 +0900

    Sequel::Model.def_dataset_method move to def_dataset_method plugin
    - https://github.com/jeremyevans/sequel/blob/5.2.0/CHANGELOG#L407
    - https://github.com/jeremyevans/sequel/commit/c385c51097787aa4fc4bd3095a4018e7a20a431c#diff-49a3ae916a87351503eee632e096e4a1

--- lib/sequel/plugins/enumerize.rb
+++ lib/sequel/plugins/enumerize.rb
@@ -1,6 +1,11 @@
 module Sequel
   module Plugins
     module Enumerize
+      # Depend on the def_dataset_method plugin
+      def self.apply(model)
+        model.plugin(:def_dataset_method) unless model.respond_to?(:def_dataset_method)
+      end
+
       module InstanceMethods
         def self.included(base)
           base.extend ::Enumerize
