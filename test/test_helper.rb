e034116b658b802d7a50dc5b630e4783f78d7db5
Vasiliy Ermolovich <younash@gmail.com>
2023-08-13 21:58:19 +0200

    Require ActiveJob for mongoid.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require 'minitest/autorun'
 require 'minitest/spec'
 require 'active_support/core_ext/kernel/reporting'
 require 'active_model'
+require 'active_job'
 require 'rails'
 begin
   require 'mongoid'
